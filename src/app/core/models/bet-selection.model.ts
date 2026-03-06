46c5f60fb0c1ac9c9efbc80148cc8c139d98ba88
bama.abougou <bama.anne-marie.abougou@devoteam.com>
2026-03-06 14:49:31 +0100

    ajout composant match hml css et selection pari

---
+++ src/app/core/models/bet-selection.model.ts
@@ -0,0 +1,20 @@
+export type TypePari = 'homeWin' | 'draw' | 'awayWin';
+
+export interface BetSelection {
+  matchId: string;
+  matchLabel: string; //le match exemple Paris vs Marseille
+  typePari: TypePari;//type de pari
+  libellePari: string;
+  cote: number;//cote du pari que l'utilisateur a sélectionné
+}
+
+//exemple 
+/*
+{
+  matchId: '1',
+  matchLabel: 'PSG vs Marseille',
+  outcome: 'homeWin',
+  outcomeLabel: 'PSG',
+  odd: 1.85
+}
+*/
--- src/app/features/match-list/match-list.ts
+++ src/app/features/match-list/match-list.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Match } from '../../core/models/match.model';
+import { TypePari } from '../../core/models/bet-selection.model';
+import { MATCHES_MOCK } from '../../core/mocks/match.mock';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-match-list',
   standalone:true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './match-list.html',
   styleUrl: './match-list.scss',
 })
 export class MatchList {
+
+  protected readonly matches: Match[] = MATCHES_MOCK;
+
+  selectOdd(match: Match, typePari: TypePari) {
+  const pariSelected =  {
+    matchId: match.id,
+    teams: `${match.teams.a} vs ${match.teams.b}`,
+    typePari : typePari
+  };
+}
 
 }
