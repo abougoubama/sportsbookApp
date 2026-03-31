ebd2d302f6c46ffb6716bc6a44f816e8eaf3d321
bama.abougou <bama.anne-marie.abougou@devoteam.com>
2026-03-31 22:21:41 +0200

    ajout record pour recuperer libelle et equipe

--- src/app/features/match-list/match-list.ts
+++ src/app/features/match-list/match-list.ts
@@ -7,10 +7,11 @@ import { MatchService } from '../../core/services/match';
 import { BetSlipService } from '../../core/services/bet-slip';
 import { MATCH_LIST_LABELS } from '../../constants/constants-sportBookApp';
 
+type CoteInfo = { libelleEquipe: string; cote: number };
 
 @Component({
   selector: 'app-match-list',
-  standalone:true,
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './match-list.html',
   styleUrl: './match-list.scss',
@@ -18,55 +19,63 @@ import { MATCH_LIST_LABELS } from '../../constants/constants-sportBookApp';
 export class MatchList implements OnInit {
   matchListlabels = MATCH_LIST_LABELS;
 
-  public  matches$!:Observable<Match[]>;
+  public matches$!: Observable<Match[]>;
   public readonly selectionsByMatchId: Record<string, BetSelection> = {};
 
-  constructor(private readonly matchService:MatchService, private readonly betSlipService:BetSlipService) {
-
-   }
+  constructor(
+    private readonly matchService: MatchService,
+    private readonly betSlipService: BetSlipService,
+  ) {}
 
   ngOnInit() {
     this.matches$ = this.matchService.getMatches();
-
   }
 
-  getCote(match: Match, typePari: TypePari):{libelleEquipe:string,cote:number}{
-    if(typePari === "awayWin"){
-      return { libelleEquipe: match.teams.b,cote:match.odds.awayWin}
-    }else if(typePari === "draw"){
-      return { libelleEquipe: typePari,cote: match.odds.draw?match.odds.draw:0};
-    }else{
-      return { libelleEquipe: match.teams.a,cote: match.odds.homeWin};
-    }
+  readonly coteResolvers: Record<TypePari, (match: Match) => CoteInfo> = {
+    awayWin: (match) => ({
+      libelleEquipe: match.teams.b,
+      cote: match.odds.awayWin,
+    }),
+    draw: (match) => ({
+      libelleEquipe: 'draw',
+      cote: match.odds.draw ?? 0,
+    }),
+    homeWin: (match) => ({
+      libelleEquipe: match.teams.a,
+      cote: match.odds.homeWin,
+    }),
+  };
+
+  getCote(match: Match, typePari: TypePari): { libelleEquipe: string; cote: number } {
+    const resolver = this.coteResolvers[typePari];
+    return resolver(match);
   }
 
-  selectOdd(match: Match, typePari: TypePari):void {
+  selectOdd(match: Match, typePari: TypePari): void {
     const cote = this.getCote(match, typePari).cote;
     const currentSelection = this.betSlipService.getSelectionPariValue();
     const betSelection = {
-    matchId: match.id,
-    matchLabel: match.competition,
-    typePari: typePari,
-    libellePari: match.teams.a + " vs " + match.teams.b,
-    libelleCote: this.getCote(match, typePari).libelleEquipe,
-    cote: this.getCote(match, typePari).cote,
-    gain: (currentSelection[match.id]?.mise ?? 0) * cote,
-    mise: currentSelection[match.id]?.mise ?? 0,
-    misMax: false
-  }
+      matchId: match.id,
+      matchLabel: match.competition,
+      typePari: typePari,
+      libellePari: match.teams.a + ' vs ' + match.teams.b,
+      libelleCote: this.getCote(match, typePari).libelleEquipe,
+      cote: this.getCote(match, typePari).cote,
+      gain: (currentSelection[match.id]?.mise ?? 0) * cote,
+      mise: currentSelection[match.id]?.mise ?? 0,
+      misMax: false,
+    };
 
-  if(currentSelection[match.id] === undefined){
-    this.selectionsByMatchId[match.id] = betSelection;
-  }else{
-    const pariCurrentMatch =currentSelection[match.id].typePari;
-    if(pariCurrentMatch === typePari){
-      delete this.selectionsByMatchId[match.id];
-    }else{
+    if (currentSelection[match.id] === undefined) {
       this.selectionsByMatchId[match.id] = betSelection;
+    } else {
+      const pariCurrentMatch = currentSelection[match.id].typePari;
+      if (pariCurrentMatch === typePari) {
+        delete this.selectionsByMatchId[match.id];
+      } else {
+        this.selectionsByMatchId[match.id] = betSelection;
+      }
     }
-
-  }
     this.betSlipService.setSelectionPari(this.selectionsByMatchId);
-}
-
+  }
 }
